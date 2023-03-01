627a1d714055b548a24398b408950299892b08c8
brGuirra <br.guirra@gmail.com>
2023-03-01 00:17:05 -0300

    feat: add crud operations to database with sqlc

--- db/migration/000001_init_schema.up.sql
+++ db/migration/000001_init_schema.up.sql
@@ -1,24 +1,24 @@
 CREATE TABLE "accounts" (
-                            "id" bigserial PRIMARY KEY,
-                            "owner" varchar NOT NULL,
-                            "balance" bigint NOT NULL,
-                            "currency" varchar NOT NULL,
-                            "created_at" timestamptz NOT NULL DEFAULT (now())
+  "id" bigserial PRIMARY KEY,
+  "owner" varchar NOT NULL,
+  "balance" bigint NOT NULL,
+  "currency" varchar NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (NOW())
 );
 
 CREATE TABLE "entries" (
-                           "id" bigserial PRIMARY KEY,
-                           "account_id" bigserial NOT NULL,
-                           "amount" bigint NOT NULL,
-                           "created_at" timestamptz NOT NULL DEFAULT (now())
+  "id" bigserial PRIMARY KEY,
+  "account_id" bigserial NOT NULL,
+  "amount" bigint NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (NOW())
 );
 
 CREATE TABLE "transfers" (
-                             "id" bigserial PRIMARY KEY,
-                             "from_account_id" bigserial NOT NULL,
-                             "to_account_id" bigserial NOT NULL,
-                             "amount" bigint NOT NULL,
-                             "created_at" timestamptz NOT NULL DEFAULT (now())
+  "id" bigserial PRIMARY KEY,
+  "from_account_id" bigserial NOT NULL,
+  "to_account_id" bigserial NOT NULL,
+  "amount" bigint NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (NOW())
 );
 
 CREATE INDEX ON "accounts" ("owner");
@@ -35,8 +35,11 @@ COMMENT ON COLUMN "entries"."amount" IS 'Can be negative or positive';
 
 COMMENT ON COLUMN "transfers"."amount" IS 'Must be positive';
 
-ALTER TABLE "entries" ADD FOREIGN KEY ("account_id") REFERENCES "accounts" ("id");
+ALTER TABLE "entries"
+ADD FOREIGN KEY ("account_id") REFERENCES "accounts" ("id");
 
-ALTER TABLE "transfers" ADD FOREIGN KEY ("from_account_id") REFERENCES "accounts" ("id");
+ALTER TABLE "transfers"
+ADD FOREIGN KEY ("from_account_id") REFERENCES "accounts" ("id");
 
-ALTER TABLE "transfers" ADD FOREIGN KEY ("to_account_id") REFERENCES "accounts" ("id");
+ALTER TABLE "transfers"
+ADD FOREIGN KEY ("to_account_id") REFERENCES "accounts" ("id");
--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
-module simple_bank
+module github.com/brGuirra/simple-bank
 
 go 1.20
-
-require github.com/joho/godotenv v1.5.1 // indirect
